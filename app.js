4bd0fc71f4f920ca4287398c8d792f94061fa1ec
Lyudmyla Tkachenko <luda_tkach@yahoo.com>
2020-01-14 10:31:22 -0800

    initial setup

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+// https://stackoverflow.com/questions/7486623/mongodb-password-with-in-it
+mongoose.connect('mongodb+srv://node-shop:node-shop%40luda@node-rest-shop-4frjd.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+});
+mongoose.Promise = global.Promise;
+
+app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+//Routs which should handle requests
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+//Handle error 404: didn't found the route.
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+//Handle all kind of errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    }
+  });
+});
+
+module.exports = app;
